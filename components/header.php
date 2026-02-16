ae54fdb0320cd006094aa21be4a953ce458cecec
Thunay Moreira de Soares (BX) <thunay@buildbox.com.br>
2026-02-16 12:53:34 -0300

    🚧 Header

--- components/header.php
+++ components/header.php
@@ -9,12 +9,7 @@
 
 <header class="bg-white border-b-2 border-blue-500">
     <div class="container">
-        <nav class="flex items-center justify-between">
-            <div class="flex items-center">
-                <div class="h-8 w-px bg-gray-300 mr-4"></div>
-                <img src="<?php echo get_template_directory_uri(); ?>/src/images/logo.png" alt="Logo" class="w-24 h-auto">
-                <div class="h-8 w-px bg-gray-300 ml-4"></div>
-            </div>
+        <nav>
             <div class="flex items-center flex-1 ml-6">
                 <?php
 
