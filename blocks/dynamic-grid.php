e80266cb5f376dc21e40f005be03bfad29d2957f
Thunay Moreira de Soares (BX) <thunay@buildbox.com.br>
2026-03-13 13:30:23 -0300

    🚧  Blocks: Dynamic Grid

--- blocks/dynamic-grid.php
+++ blocks/dynamic-grid.php
@@ -6,10 +6,29 @@ if (!defined('ABSPATH')) {
 
 $post_grid_layout_style = get_field('block_dynamic_post_grid_layout_style');
 $post_grid_columns      = get_field('block_dynamic_post_grid_columns') ?? 2;
+$post_grid_categories   = get_field('block_dynamic_post_grid_categories');
+$post_grid_ordenation   = get_field('block_dynamic_post_grid_ordenation');
+$post_grid_ordenation = 'recent';
+
+$posts_list_args = [
+    'numberposts' => $post_grid_columns,
+    'post_type'   => 'post',
+];
+
+if (!empty($post_categories)) {
+    $posts_list_args['category'] = implode(', ', $post_categories);
+}
+
+// TODO: CREATE QUERY FOR MOST READ
+if ($post_grid_ordenation === 'recent') {
+    $posts_list_args['orderby'] = 'date';
+    $posts_list_args['order']   = 'DESC';
+}
 
 if ($post_grid_layout_style === 'default') {
     get_template_part('components/grid', 'default', [
-        'grid_columns' => $post_grid_columns,
+        'post_grid_columns' => $post_grid_columns,
+        'posts_list_args'   => $posts_list_args
     ]);
 }
 
--- components/grid-default.php
+++ components/grid-default.php
@@ -1,1 +1,30 @@
-<?php echo 'Default grid';
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+$post_grid_columns = $args['post_grid_columns'] ?? 2;
+$posts_list_args   = $args['posts_list_args'];
+
+$posts = get_posts($posts_list_args);
+
+if (empty($posts)) {
+    return;
+}
+
+$grid_columns_classes = [
+    2 => 'lg:grid-cols-2',
+    3 => 'lg:grid-cols-3',
+    4 => 'lg:grid-cols-4',
+];
+
+$list_output = '';
+
+$list_output .= $grid_columns_classes[$post_grid_columns ?? 2];
+
+?>
+
+<ul class="grid grid-cols-1 gap-8 <?php echo esc_attr($list_output); ?>">
+
+</ul>
